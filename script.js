3161184c35ebaf7e1aa8a74ee13c4568d3a43b7a
Yuls <ulyssesmarcorde@gmail.com>
2025-12-12 10:22:57 +0800

    yo

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("header").innerHTML = data;
         });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+});
+
+
+
+const slide = document.querySelector(".carousel-slide");
+const images = document.querySelectorAll(".carousel-slide img");
+
+let counter = 0;
+
+document.querySelector(".next").addEventListener("click", () => {
+  counter++;
+  if (counter >= images.length) counter = 0;
+  updateSlide();
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+  counter--;
+  if (counter < 0) counter = images.length - 1;
+  updateSlide();
+});
+
+function updateSlide() {
+  const width = document.querySelector(".carousel-container").clientWidth;
+  slide.style.transform = `translateX(${-counter * width}px)`;
+}
+
+window.addEventListener("resize", updateSlide);
